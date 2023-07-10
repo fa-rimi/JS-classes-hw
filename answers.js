0af09718f84b76d72b051f3852899b0a76964c19
rimi <a.fatimaarimi@gmail.com>
2023-07-10 14:46:46 -0400

    completed cats

--- answers.js
+++ answers.js
@@ -1,7 +1,33 @@
-class Cat {}
-
-class pirate {
-    constructor() {
-        
+class Cat{
+    constructor(name, coat, kitten) {
+        this.name = name;
+        this.coat = coat;
+        this.kitten = kitten;
+    }
+    age(){
+        if (this.kitten === true) {
+            console.log('and i\'m just a baby');
+        } else {
+            console.log('and I am NOT a baby');
+        }
+    }
+    call(){
+        console.log(`My name is ${this.name}`);
+    }
+    pattern(){
+        console.log(`I am a ${this.coat}`);
     }
 }
+
+const nala = new Cat('Nala', 'Tortoiseshell', true);
+const mini = new Cat( 'Mini', 'Tuxedo', false);
+
+console.log(nala);
+nala.call();
+nala.pattern();
+nala.age();
+
+console.log(mini);
+mini.call();
+mini.pattern();
+mini.age();
