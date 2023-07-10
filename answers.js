49ff2d5883762919fc94f44b502b1e58a95d2b43
rimi <a.fatimaarimi@gmail.com>
2023-07-10 15:25:47 -0400

    pirate contructors sections

--- answers.js
+++ answers.js
@@ -1,26 +1,27 @@
-class Cat{
-    constructor(name, coat, kitten) {
-        this.name = name;
-        this.coat = coat;
-        this.kitten = kitten;
+// CREATING CLASSES
+class Cat {
+  constructor(name, coat, kitten) {
+    this.name = name;
+    this.coat = coat;
+    this.kitten = kitten;
+  }
+  age() {
+    if (this.kitten === true) {
+      console.log("and i'm just a baby");
+    } else {
+      console.log("and I am NOT a baby");
     }
-    age(){
-        if (this.kitten === true) {
-            console.log('and i\'m just a baby');
-        } else {
-            console.log('and I am NOT a baby');
-        }
-    }
-    call(){
-        console.log(`My name is ${this.name}`);
-    }
-    pattern(){
-        console.log(`I am a ${this.coat}`);
-    }
+  }
+  call() {
+    console.log(`My name is ${this.name}`);
+  }
+  pattern() {
+    console.log(`I am a ${this.coat}`);
+  }
 }
 
-const nala = new Cat('Nala', 'Tortoiseshell', true);
-const mini = new Cat( 'Mini', 'Tuxedo', false);
+const nala = new Cat("Nala", "Tortoiseshell", true);
+const mini = new Cat("Mini", "Tuxedo", false);
 
 console.log(nala);
 nala.call();
@@ -31,3 +32,58 @@ console.log(mini);
 mini.call();
 mini.pattern();
 mini.age();
+
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// CONSTRUCTORS
+class pirates {
+  constructor(name, goal, food, ship) {
+    this.name = name;
+    this.goal = goal;
+    this.food = food;
+    this.ship = ship;
+  }
+  iAm() {
+    if (this.goal === true) {
+      console.log("I\'m finding the one piece!");
+    } else {
+      console.log("I\'m just cruising...");
+    }
+  }
+  call() {
+    console.log(`My name is ${this.name}`);
+  }
+  hungry() {
+    console.log(`I like ${this.food}`);
+  }
+  boatName() {
+    console.log(`My ship name is ${this.ship}`);
+  }
+}
+
+const pirate1 = new pirates(
+  "Luffy",
+  true,
+  "Meat Meat and More Meat",
+  "The Thousand Sunny"
+);
+const pirate2 = new pirates("Nami", false, "Tangerines", "LuNa");
+const pirate3 = new pirates("Jack Sparrow", false, "Rum", "The Black Pearl");
+
+console.log(pirate1);
+pirate1.call();
+pirate1.boatName();
+pirate1.iAm();
+pirate1.hungry();
+
+console.log(pirate2);
+pirate2.call();
+pirate2.boatName();
+pirate2.iAm();
+pirate2.hungry();
+
+console.log(pirate3);
+pirate3.call();
+pirate3.boatName();
+pirate3.iAm();
+pirate3.hungry();
